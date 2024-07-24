a61691f8b52f0966939c5cb548bef98d1b3d3c72
Nikhil Kaswan <112556382+nikhilkaswann@users.noreply.github.com>
2024-07-24 21:59:35 +0530

    Update allowedOrigins.js

--- blogging-website-backend/config/allowedOrigins.js
+++ blogging-website-backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://loquacious-seahorse-614b70.netlify.app/'
+    'https://strong-torte-14f816.netlify.app/'
 
 ];
 
